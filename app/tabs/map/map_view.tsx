5b25d1c6276245aba9e69ce63e1226fc535252a5
Ansh Singh <anshdaboss@gmail.com>
2025-06-12 11:12:53 -0500

    style updates

--- app/tabs/map/map_view.tsx
+++ app/tabs/map/map_view.tsx
@@ -1,3 +1,4 @@
+import { MaterialIcons } from '@expo/vector-icons';
 import * as Location from 'expo-location';
 import { Stack } from "expo-router";
 import React, { useEffect, useRef, useState } from "react"; //useEffect
@@ -11,11 +12,6 @@ export default function GeoMap() {
     latitudeDelta: 35,
     longitudeDelta: 35,
   });
-
-  const [chosenType, setMapType] = useState({
-    type: MAP_TYPES.STANDARD, // STANDARD, HYBRID, TERRAIN, SATELLITE (compatible with Android and iOS)
-  });
-
   const mapRef = useRef<MapView | null>(null);
   const userLocation = async () => { // Retrieve user's current location function
     let { status } = await Location.requestForegroundPermissionsAsync();
@@ -33,22 +29,50 @@ export default function GeoMap() {
     }, 1000);
   }
 
+  const [currentRegion, setCurrentRegion] = useState({}); // The region that the user is currently looking at (lat, long, latDelta, longDelta)
+  const handleRegionChangeComplete = (region: React.SetStateAction<{}>) => {
+    setCurrentRegion(region);
+
+    console.log('Current Region:', region); 
+  };
+
+
+  // Map types:
+  const terrain_types = [MAP_TYPES.STANDARD, MAP_TYPES.TERRAIN, MAP_TYPES.HYBRID,  MAP_TYPES.SATELLITE]; // Map types compatible with Android and iOS
+  const [chosenType, setMapType] = useState({
+    type: terrain_types[0], // Default map type is standard
+  });
+  const changeMapType = () => { 
+    let currentIndex = terrain_types.indexOf(chosenType.type); // Get the current index of the map type
+    if (currentIndex < terrain_types.length - 1) { // If not the last type
+      setMapType({ type: terrain_types[currentIndex + 1] });
+    } else { // If the last type, reset to the first type
+      setMapType({ type: terrain_types[0] });
+    }
+  }
+
+
+
   useEffect(() => {
     userLocation()
-  },[]); // empty brackets signifies that this function runs only once when the page renders
+  },[]); // empty brackets means that this function runs only once when the page renders
 
   return (
 <>
     <Stack.Screen options={{ title: "Event Map", headerShown: false }}/> 
     <View style={{ flex: 1 }}>
-      <MapView style={{ width: '100%', height: '100%' }} ref={mapRef} provider={PROVIDER_GOOGLE} initialRegion={mapRegion} mapType={chosenType.type} showsUserLocation={true} />
-      <TouchableOpacity style={{ position: 'absolute', top: 500, right: 300, backgroundColor: 'green', borderRadius: 5, padding: 50 }} onPress={userLocation} ></TouchableOpacity>
-      <TouchableOpacity style={{ position: 'absolute', top: 500, right: 100, backgroundColor: 'black', borderRadius: 5, padding: 50 }} onPress={() => setMapType({ type: MAP_TYPES.HYBRID })}></TouchableOpacity>
+      <MapView style={{ width: '100%', height: '100%' }} ref={mapRef} provider={PROVIDER_GOOGLE} initialRegion={mapRegion} onRegionChangeComplete={handleRegionChangeComplete} mapType={chosenType.type} showsUserLocation={true} />
+        <View style={styles.mapOptionsContainer}>
+          
+          <TouchableOpacity style={styles.mapOptions} onPress={userLocation}><MaterialIcons name="my-location" size={33} color="white" /></TouchableOpacity>
+          <TouchableOpacity style={styles.mapOptions} onPress={changeMapType}><MaterialIcons name="map" size={33} color="white" /></TouchableOpacity>
+        </View>
+      <MapView/>
       <MapView/>
     </View>
 </>
-  // NEXT: FIND A WAY TO FIND ELEVATION FROM GROUND or sea level, IF HIGHER THAN A CERTAIN ELEVATION, SHOW TOP 5 EVENTS FROM USERS CURRENT VIEW OF THE MAP, OTHERWISE..
-  // .. IF UNDER ELEVATION THRESHOLD (like looking close up at a city), SHOW 10(?) EVENTS IN THE CITY/LOCATION THEY'RE LOOKING AT.
+  // NEXT: FIND A WAY TO FIND ELEVATION FROM GROUND or sea level, IF HIGHER THAN A CERTAIN ELEVATION, SHOW TOP 10 EVENTS FROM USERS CURRENT VIEW OF THE MAP, OTHERWISE..
+  // .. IF UNDER ELEVATION THRESHOLD (like looking close up at a city), SHOW 20(?) EVENTS IN THE CITY/LOCATION THEY'RE LOOKING AT.
 
       // {/* <Stack.Screen options={{ headerShown: false }}/>
       // <View style={styles.container}>
@@ -68,6 +92,27 @@ export default function GeoMap() {
 }
 
 const styles = StyleSheet.create({
+  mapOptionsContainer: {
+    position: 'absolute', 
+    bottom: 10, 
+    right: 10,
+    width: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  mapOptions: {
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'green', 
+    borderRadius: 100, //circle
+    width: 50,
+    height: 50,
+    marginBottom: 10,
+    opacity: 0.8,
+  },
+
   container: {
     flex: 1,
     justifyContent: "center",
